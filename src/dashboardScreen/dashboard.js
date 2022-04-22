c019984e05f37cfcd147b98047f32896ef60aeb1
Jason <jason231.az@gmail.com>
2022-04-21 22:25:17 -0700

    Fixed Transition to Board

--- src/dashboardScreen/dashboard.js
+++ src/dashboardScreen/dashboard.js
@@ -6,3 +6,13 @@ function newGame()
         window.open("../boardScreen/index.html", "_self");
     },4000);
 }
+
+document.getElementById("matches").addEventListener("click", existingRound);
+function existingRound()
+{
+    document.getElementById("newGameIcon").classList.add("newGameTransition");
+    setTimeout(() => {
+        window.open("../boardScreen/index.html", "_self");
+    },4000);
+}
+
